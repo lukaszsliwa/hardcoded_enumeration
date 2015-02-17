5c3d0529bd5ef395ef9b447d3d5bbc4ab48b9088
Łukasz Śliwa <lukasz.sliwa@apptamers.com>
2015-02-17 18:31:30 +0100

    Reorganized dirs

--- init.rb
+++
@@ -1,5 +0,0 @@
-require 'hardcoded_enumeration/base'
-require 'hardcoded_enumeration/active_record_extensions'
-ActiveRecord::Base.class_eval do
-  extend HardcodedEnumeration::ActiveRecordExtensions
-end
